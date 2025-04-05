684aa47c6f5e3732c37749c633c9f3fa3a6a0822
Filip Matejko <matejkofilip@gmail.com>
2025-04-05 11:14:22 +0200

    finalizing the correct cicd I need

--- tests/module_tests/test_dumdum.py
+++ tests/module_tests/test_dumdum.py
@@ -1,4 +1,7 @@
-
+import os
 
 def test_pass():
-  pass
+  os.mkdir('logs-module')
+  with open('logs-module/some_art.txt', 'w') as artefact:
+    artefact.write('test got errORRRRREEEEEEEE!')
+  raise Exception('REEEEEEE!!!')
