7b1e783b3b0b4eee83711675b03e0df899ded242
Filip Matejko <matejkofilip@gmail.com>
2025-04-05 13:07:20 +0200

    reusing wf in release

--- tests/module_tests/test_dumdum.py
+++ tests/module_tests/test_dumdum.py
@@ -3,5 +3,5 @@ import os
 def test_pass():
   os.mkdir('logs-module')
   with open('logs-module/some_art.txt', 'w') as artefact:
-    artefact.write('test got errORRRRREEEEEEEE!')
-  raise Exception('REEEEEEE!!!')
+    artefact.write('test got no errORRRRREEEEEEEE!')
+  pass
